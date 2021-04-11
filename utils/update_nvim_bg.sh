394e2998dd018ea0b3b30c2aa1ad8e7c8f169d47
James Stevenson <james.sharpsteen@gmail.com>
2021-04-11 08:10:45 -0400

    Fix function name

--- utils/update_nvim_bg.sh
+++ utils/update_nvim_bg.sh
@@ -2,6 +2,6 @@
 
 for _pane in $(tmux list-panes -a -F '#{pane_id}'); do
     if [[ $(tmux display-message -t $_pane -p -F '#{pane_current_command}') = "nvim" ]]; then
-        tmux send-keys -t $_pane Escape ':call SetBackground()' Enter
+        tmux send-keys -t $_pane Escape ':call SetTheme()' Enter
     fi
 done
