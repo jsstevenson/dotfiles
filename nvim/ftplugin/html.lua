1b7ae1d60eefa96d25ddcd373bb6f697cfdf8b34
James Stevenson <james.stevenson@nationwidechildrens.org>
2022-04-03 17:02:19 -0400

    Trying to get HTML spacing correct

--- nvim/ftplugin/html.lua
+++ nvim/ftplugin/html.lua
@@ -1,2 +1,9 @@
 vim.g.html_indent_inctags = 'html,body,head,tbody,div'
 vim.g.html_indent_script1 = 'inc'
+vim.b.softtabstop = 2
+vim.b.shiftwidth = 2
+vim.b.tabstop = 2
+
+-- opt('b', 'shiftwidth', 4)
+-- opt('b', 'softtabstop', 4)
+
