1af3bd86ae75dda3ad962f100b265b4768606452
James Stevenson <james.stevenson@nationwidechildrens.org>
2022-08-09 21:53:01 -0400

    Fix filetype bug

--- nvim/ftdetect/tsv.lua
+++ nvim/ftdetect/tsv.lua
@@ -1,6 +1,7 @@
 local group = vim.api.nvim_create_augroup("SetTSVFiletype", { clear = true })
 vim.api.nvim_create_autocmd({ "BufEnter", "BufNewFile" }, {
   group = group,
+  pattern = "*.tsv",
   callback = function()
     vim.bo.filetype = "tsv"
   end,
