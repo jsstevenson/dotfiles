d31dcf0adb7c3455a96431127af523d36d26c74b
James Stevenson <james.stevenson@nationwidechildrens.org>
2022-09-13 21:36:55 -0400

    fix typo

--- nvim/ftplugin/rst.lua
+++ nvim/ftplugin/rst.lua
@@ -1,4 +1,4 @@
 -- header shortcuts
 local mapx = require("mapx")
-mapx.nnoremap("<leader>=", "yypv$r=", "silet")
-mapx.nnoremap("<leader>-", "yypv$r-", "silet")
+mapx.nnoremap("<leader>=", "yypv$r=", "silent")
+mapx.nnoremap("<leader>-", "yypv$r-", "silent")
