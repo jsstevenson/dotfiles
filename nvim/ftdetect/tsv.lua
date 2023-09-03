c8d78d09de056dfff41fe9027acd86bf3c884dc1
James Stevenson <james.stevenson@nationwidechildrens.org>
2023-09-02 20:49:55 -0400

    tmp tsv ftdetect fix

--- nvim/ftdetect/tsv.lua
+++ nvim/ftdetect/tsv.lua
@@ -1,7 +1,8 @@
-local group = vim.api.nvim_create_augroup("SetTSVFiletype", { clear = true })
-vim.api.nvim_create_autocmd({ "BufEnter", "BufNewFile" }, {
-  group = group,
-  callback = function()
-    vim.bo.filetype = "tsv"
-  end,
-})
+-- TODO maybe this worked at some point, but currently, it is defining every file as a TSV
+-- local group = vim.api.nvim_create_augroup("SetTSVFiletype", { clear = true })
+-- vim.api.nvim_create_autocmd({ "BufEnter", "BufNewFile" }, {
+--   group = group,
+--   callback = function()
+--     vim.bo.filetype = "tsv"
+--   end,
+-- })
