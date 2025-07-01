0b0860c73ab52075a3a74f609598410b96660340
James Stevenson <james.stevenson@nationwidechildrens.org>
2025-06-30 21:46:53 -0400

    seemingly working formatting implementation

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,14 +1,19 @@
 -- TODO
--- * figure out format
+-- * figure out autoimport
 -- * reload LSP command
+-- * figure out snippets
+-- * would be nice to see if there's a way to identify configs being used
 local Plugin = { "neovim/nvim-lspconfig" }
 
 Plugin.dependencies = {
-  { "mason-org/mason.nvim", opts = {} },
+  { "mason-org/mason.nvim", opts = {}, cmd = { "Mason" } },
   { "mason-org/mason-lspconfig.nvim", opts = {} },
+  { "stevearc/conform.nvim", opts = {} },
   { "smjonas/inc-rename.nvim" },
+  { "stevearc/conform.nvim" },
+  { "zapling/mason-conform.nvim" },
 }
-Plugin.cmd = { "LspInfo", "LspInstall", "LspUnInstall", "Mason" }
+Plugin.cmd = { "LspInfo", "LspInstall", "LspUnInstall" }
 Plugin.event = { "BufReadPre", "BufNewFile" }
 
 function Plugin.init()
@@ -33,7 +38,21 @@ function Plugin.config()
     },
   })
   require("mason-lspconfig").setup({
-    ensure_installed = { "pyright", "rust_analyzer", "lua_ls", "ruff", "biome", "ruby_lsp" },
+    ensure_installed = { "pyright", "ruff", "biome", "ruby_lsp", "rust_analyzer", "lua_ls" },
+  })
+
+  require("plugins.lsp.format")
+
+  vim.lsp.config("luals", {
+    settings = {
+      ["luals"] = {
+        capabilities = {
+          textDocument = {
+            formatting = false,
+          },
+        },
+      },
+    },
   })
 
   require("inc_rename").setup({ preview_empty_name = true })
@@ -42,23 +61,18 @@ function Plugin.config()
     desc = "LSP actions",
     group = vim.api.nvim_create_augroup("lsp.config", {}),
     callback = function(args)
-      local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
       local opts = { buffer = args.buf }
-      -- vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-      vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-      vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-      vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-      vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-      vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-      vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-      if client:supports_method("textDocument/formatting") then
-        vim.keymap.set({ 'n', 'x' }, '<leader>f', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-      end
-      -- vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+      vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+      vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+      vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+      vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
+      vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
+      vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+      vim.keymap.set("n", "<leader>a", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
       vim.keymap.set("n", "<leader>r", function()
         return ":IncRename " .. vim.fn.expand("<cword>")
       end, { expr = true })
-    end
+    end,
   })
 end
 
---
+++ nvim/lua/plugins/lsp/format.lua
@@ -0,0 +1,20 @@
+local conform = require("conform")
+conform.setup({
+  default_format_opts = {
+    lsp_format = "fallback",
+  },
+  format_on_save = {
+    lsp_format = "fallback",
+    timeout_ms = 500,
+  },
+  formatters_by_ft = {
+    lua = { "stylua", lsp_format = "never" },
+    rust = { "rustfmt", lsp_format = "never" },
+    json = { "biome", lsp_format = "never" },
+    python = { "ruff_format", lsp_format = "never" },
+  },
+})
+vim.keymap.set("n", "<Leader>f", function()
+  conform.format({ async = true, lsp_fallback = true })
+end)
+require("mason-conform").setup({ ignore_install = { "ruff", "biome" } })
