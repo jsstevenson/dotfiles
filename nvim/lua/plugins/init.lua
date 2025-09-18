af90b30355979796925373934e7dbf2e8d63a606
James Stevenson <james.stevenson@nationwidechildrens.org>
2025-09-17 22:59:22 -0400

    feat: add kulala.nvim

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -13,6 +13,14 @@ local Plugins = {
   { "kylechui/nvim-surround", opts = {} },
   { "lewis6991/gitsigns.nvim", opts = {} },
   { "rhysd/conflict-marker.vim" },
+  {
+    "mistweaverco/kulala.nvim",
+    opts = {
+      global_keymaps = true,
+      global_keymaps_prefix = "<leader>R",
+      kulala_keymaps_prefix = "",
+    },
+  },
 }
 
 return Plugins
